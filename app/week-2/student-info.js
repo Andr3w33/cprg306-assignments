28882ab5db34ced1c103d41c28d82df090080538
Andrew Le <95255393+Andr3w33@users.noreply.github.com>
2025-09-17 20:06:55 -0600

    Create student-info.js

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+function StudentInfo() {
+  return (
+    <nav>
+      <p>Andrew Le</p>
+      <Link href="https://github.com/Andr3w33/cprg306-assignments">GitHub: Andr3w33/cprg306-assignments</Link>
+    </nav>
+  );
+}
+
+export default StudentInfo;
